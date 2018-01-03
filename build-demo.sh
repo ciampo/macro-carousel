68b4bdaff53eb01d5d31e505636df4c467c703c9
Marco Ciampini <marco.ciampo@gmail.com>
2018-01-03 14:59:18 +0100

    GH-3 feat(a11y): added WICG inter and focus-ring polyfills as peer deps

--- build-demo.sh
+++ build-demo.sh
@@ -2,3 +2,9 @@
 
 # Add WC polyfill loader
 cp node_modules/@webcomponents/webcomponentsjs/*.js demo/
+
+# Add inert polyfill
+cp node_modules/wicg-inert/dist/inert.min.js demo/
+
+# Add focus-ring polyfill
+cp node_modules/wicg-focus-ring/dist/focus-ring.js demo/
