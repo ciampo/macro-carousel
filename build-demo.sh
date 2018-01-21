c699d7c425331155ae298a987f8943b795b39169
Marco Ciampini <marco.ciampo@gmail.com>
2018-01-21 12:14:50 +0000

    refactor(demo): moved js files into js/ folder

--- build-demo.sh
+++ build-demo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+mkdir -p demo/js
 
 # Add WC polyfill loader
-cp node_modules/@webcomponents/webcomponentsjs/*.js demo/
+cp node_modules/@webcomponents/webcomponentsjs/*.js demo/js/
 
 # Add inert polyfill
-cp node_modules/wicg-inert/dist/inert.min.js demo/
+cp node_modules/wicg-inert/dist/inert.min.js demo/js/
 
 # Add focus-visible polyfill
-cp node_modules/focus-visible/dist/focus-visible.js demo/
+cp node_modules/focus-visible/dist/focus-visible.js demo/js/
