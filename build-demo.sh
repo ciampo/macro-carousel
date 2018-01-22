f7876ce38a5f01d3804d001912eb7d75d6d79445
Marco Ciampini <marco.ciampo@gmail.com>
2018-01-22 23:53:24 +0000

    GH-10 refactor(demo): moving external JS into third-party folder

--- build-demo.sh
+++ build-demo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mkdir -p demo/js
+mkdir -p demo/js/third-party
 
 # Add WC polyfill loader
-cp node_modules/@webcomponents/webcomponentsjs/*.js demo/js/
+cp node_modules/@webcomponents/webcomponentsjs/*.js demo/js/third-party
 
 # Add inert polyfill
-cp node_modules/wicg-inert/dist/inert.min.js demo/js/
+cp node_modules/wicg-inert/dist/inert.min.js demo/js/third-party
 
 # Add focus-visible polyfill
-cp node_modules/focus-visible/dist/focus-visible.js demo/js/
+cp node_modules/focus-visible/dist/focus-visible.js demo/js/third-party
