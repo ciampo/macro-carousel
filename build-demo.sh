b9d850f3d03142e9f8502b8248e25412166bce73
Marco Ciampini <mciampini@google.com>
2018-01-24 19:10:08 +0000

    GH-10 feat(demo): using prism.js to highlight syntax. Using templates to use the same code both for execution and syntax highlighting

--- build-demo.sh
+++ build-demo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p demo/js/third-party
+mkdir -p demo/css/third-party
 
 # Add WC polyfill loader
 cp node_modules/@webcomponents/webcomponentsjs/*.js demo/js/third-party
@@ -9,3 +10,7 @@ cp node_modules/wicg-inert/dist/inert.min.js demo/js/third-party
 
 # Add focus-visible polyfill
 cp node_modules/focus-visible/dist/focus-visible.js demo/js/third-party
+
+# Add prism.js
+cp node_modules/prismjs/prism.js demo/js/third-party
+cp node_modules/prismjs/themes/prism.css demo/css/third-party
