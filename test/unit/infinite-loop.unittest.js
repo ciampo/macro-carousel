08f50b038184b911088a75ce77a1daaf6380a7b6
Marco Ciampini <marco.ciampo@gmail.com>
2018-02-25 10:52:46 +0000

    GH-12 feat(test): infinit loop tests

---
+++ test/unit/infinite-loop.unittest.js
@@ -0,0 +1,65 @@
+/* eslint max-len: ["off"] */
+
+(function() {
+  const expect = chai.expect;
+
+  const numberOfSlides = 5;
+  const minNumberOfViewsToEnableInfiniteLoop = 3;
+
+  describe('The infinite loop functionality', function() {
+    before(wcutils.before());
+    after(wcutils.after());
+    beforeEach(async function() {
+      this.container.innerHTML = `
+      <x-slider>
+        ${[...Array(numberOfSlides).keys()]
+            .map(i => `<article>Slide ${i}</article>`)
+            .join('\n')}
+      </x-slider>`;
+      return wcutils.waitForElement('x-slider')
+        .then(() => {
+          this.slider = this.container.querySelector('x-slider');
+        });
+    });
+
+    it('is disabled when loop is disabled', async function() {
+      expect(this.slider._infiniteLoop).to.be.false;
+    });
+
+    it('is disabled when loop is enabled but there aren\'t enough slides', async function() {
+      this.slider.loop = true;
+
+      const slides = document.querySelectorAll('article');
+      for (let i = minNumberOfViewsToEnableInfiniteLoop - 1; i < numberOfSlides; i++) {
+        this.slider.removeChild(slides[i]);
+      }
+      await wcutils.flush();
+      expect(this.slider._infiniteLoop).to.be.false;
+    });
+
+    it('is disabled when loop is enabled but there aren\'t enough views', async function() {
+      this.slider.loop = true;
+      this.slider.slidesPerView = numberOfSlides - 1;
+      await wcutils.flush();
+      expect(this.slider._infiniteLoop).to.be.false;
+    });
+
+    it('is enabled when loop is enabled and there are enough slides', async function() {
+      this.slider.loop = true;
+
+      const slides = document.querySelectorAll('article');
+      for (let i = minNumberOfViewsToEnableInfiniteLoop; i < numberOfSlides; i++) {
+        this.slider.removeChild(slides[i]);
+      }
+      await wcutils.flush();
+      expect(this.slider._infiniteLoop).to.be.true;
+    });
+
+    it('is enabled when loop is enabled and there are enough views', async function() {
+      this.slider.loop = true;
+      this.slider.slidesPerView = numberOfSlides - 3;
+      await wcutils.flush();
+      expect(this.slider._infiniteLoop).to.be.true;
+    });
+  });
+})();
