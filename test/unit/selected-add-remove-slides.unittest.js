89c2306b022f8d20cf259161afdf66d91c055eee
Marco Ciampini <marco.ciampo@gmail.com>
2018-02-25 23:54:47 +0000

    GH-12 feat(test): adding/removing slides changes selected

---
+++ test/unit/selected-add-remove-slides.unittest.js
@@ -0,0 +1,48 @@
+/* eslint max-len: ["off"] */
+
+(function() {
+  const expect = chai.expect;
+
+  const numberOfSlides = 4;
+
+  describe('The value of selected', function() {
+    before(wcutils.before());
+    after(wcutils.after());
+    beforeEach(async function() {
+      this.container.innerHTML = `
+      <x-slider>
+        ${[...Array(numberOfSlides).keys()]
+            .map(i => `<article>Slide ${i}</article>`)
+            .join('\n')}
+      </x-slider>`;
+      return wcutils.waitForElement('x-slider')
+        .then(() => {
+          this.slider = this.container.querySelector('x-slider');
+          this.slides = document.querySelectorAll('article');
+        });
+    });
+
+    it('changes when adding / removing slides', async function() {
+      this.slider.selected = numberOfSlides - 1;
+      expect(this.slider.selected).to.equal(numberOfSlides - 1);
+
+      this.slider.removeChild(this.slides[0]);
+      await wcutils.flush();
+      expect(this.slider.selected).to.equal(numberOfSlides - 2);
+
+      this.slider.removeChild(this.slides[1]);
+      await wcutils.flush();
+      expect(this.slider.selected).to.equal(numberOfSlides - 3);
+
+      this.slider.appendChild(this.slides[0]);
+      this.slider.appendChild(this.slides[1]);
+      this.slider.appendChild(document.createElement('div'));
+      await wcutils.flush();
+      expect(this.slider.selected).to.equal(numberOfSlides - 3);
+
+      this.slider.selected = numberOfSlides;
+      await wcutils.flush();
+      expect(this.slider.selected).to.equal(numberOfSlides);
+    });
+  });
+})();
