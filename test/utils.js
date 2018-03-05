1b3623c1146e5d919c552242bda36dacd7e68a07
Marco Ciampini <marco.ciampo@gmail.com>
2018-03-05 23:14:51 +0000

    GH-12 feat(tests): normalizeEvent tests

--- test/utils.js
+++ test/utils.js
@@ -54,4 +54,71 @@
       });
     });
   });
+
+  const createSyntheticClickEvent = () => new MouseEvent('click', {
+    view: window,
+    bubbles: true,
+    cancelable: true,
+    clientX: window.wcutils.getRandomInt(0, 100),
+    clientY: window.wcutils.getRandomInt(0, 100),
+  });
+
+  const createSyntheticTouchEvent = (type, x, y) => {
+    const target = document.createElement('div');
+    const touchObj = new Touch({
+      identifier: Date.now(),
+      target: target,
+      clientX: x,
+      clientY: y,
+      radiusX: 2.5,
+      radiusY: 2.5,
+      rotationAngle: 10,
+      force: 0.5,
+    });
+
+    return new TouchEvent(type, {
+      cancelable: true,
+      bubbles: true,
+      touches: [touchObj],
+      targetTouches: [],
+      changedTouches: [touchObj],
+      shiftKey: true,
+    });
+  };
+
+  describe('The normalizeEvent function', function() {
+    it('correctly processes MouseEvents', function() {
+      const clickEv = createSyntheticClickEvent();
+      const normalizedEv = testFns.normalizeEvent(clickEv);
+
+      expect(normalizedEv).to.be.an('object');
+
+      expect(normalizedEv).to.have.own.property('x');
+      expect(normalizedEv).to.have.own.property('y');
+      expect(normalizedEv).to.have.own.property('id');
+      expect(normalizedEv).to.have.own.property('event');
+
+      expect(normalizedEv.x).to.equal(clickEv.clientX);
+      expect(normalizedEv.y).to.equal(clickEv.clientY);
+      expect(normalizedEv.id).to.be.null;
+      expect(normalizedEv.event).to.equal(clickEv);
+    });
+
+    it('correctly processes TouchEvents', function() {
+      const touchEv = createSyntheticTouchEvent('touchstart');
+      const normalizedEv = testFns.normalizeEvent(touchEv);
+
+      expect(normalizedEv).to.be.an('object');
+
+      expect(normalizedEv).to.have.own.property('x');
+      expect(normalizedEv).to.have.own.property('y');
+      expect(normalizedEv).to.have.own.property('id');
+      expect(normalizedEv).to.have.own.property('event');
+
+      expect(normalizedEv.x).to.equal(touchEv.changedTouches[0].clientX);
+      expect(normalizedEv.y).to.equal(touchEv.changedTouches[0].clientY);
+      expect(normalizedEv.id).to.equal(touchEv.changedTouches[0].identifier);
+      expect(normalizedEv.event).to.equal(touchEv);
+    });
+  });
 })();
